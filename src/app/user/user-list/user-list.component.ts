8d08129f9c2fa1d57fce8f4044f1c6d0edba4786
jahir509 <me.jahirahmed@gmail.com>
2023-02-02 18:27:39 +0600

    Add: User List

--- src/app/user/user-list/user-list.component.ts
+++ src/app/user/user-list/user-list.component.ts
@@ -1,26 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatTableDataSource} from "@angular/material/table";
 import {SelectionModel} from "@angular/cdk/collections";
 import {PageEvent} from "@angular/material/paginator";
+import {UserService} from "../user.service";
 
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
+export interface User {
+  user_name: string;
   country: string;
+  created_at: Date;
+  update_at: Date;
+  user_id: string;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', country: 'H'},
-  {position: 2, name: 'Helium', country: 'He'},
-  {position: 3, name: 'Lithium', country: 'Li'},
-  {position: 4, name: 'Beryllium', country: 'Be'},
-  {position: 5, name: 'Boron', country: 'B'},
-  {position: 6, name: 'Carbon',  country: 'C'},
-  {position: 7, name: 'Nitrogen',  country: 'N'},
-  {position: 8, name: 'Oxygen',  country: 'O'},
-  {position: 9, name: 'Fluorine',  country: 'F'},
-  {position: 10, name: 'Neon',  country: 'Ne'},
+const ELEMENT_DATA: User[] = [
+
 ];
 
 
@@ -31,16 +25,33 @@ const ELEMENT_DATA: PeriodicElement[] = [
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss']
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit{
 
-  totalAuthor:number = 1000;
+  totalUser:number = 1000;
   postPerPage:number = 10;
   currentPage:number = 1;
-  pageSizeOptions = [10]
+  pageSizeOptions = []
 
-  displayedColumns: string[] = ['select','action', 'position', 'name', 'country'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
+  displayedColumns: string[] = ['select','action', 'name', 'country'];
+  dataSource = new MatTableDataSource<User>();
+  selection = new SelectionModel<User>(true, []);
+
+  constructor(private userService:UserService) {
+  }
+
+  ngOnInit() {
+    this.dataSource.data = []
+    this.userService.getUsers().subscribe((response)=>{
+      if(response.status === 'OK' && response.data.length > 0 ){
+        this.dataSource.data = response.data;
+        this.totalUser = response.meta.total;
+        this.postPerPage = response.meta.perPage;
+        this.currentPage = response.meta.page;
+        this.pageSizeOptions = response.meta.postPerPage;
+      }
+    })
+  }
+
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
@@ -61,11 +72,11 @@ export class UserListComponent {
   }
 
   /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: PeriodicElement): string {
+  checkboxLabel(row?: User): string {
     if (!row) {
       return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
     }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row}`;
   }
 
   check() {
