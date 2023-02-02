1ce5f8e8c4cd91dc6e289fb12b7bb82bafd254db
jahir509 <me.jahirahmed@gmail.com>
2023-02-02 18:26:51 +0600

    Add: User Service Initially

---
+++ src/app/user/user.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+
+const apiUrl = 'https://techzuapi.azurewebsites.net/api/user'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private http: HttpClient) { }
+
+  getUsers(page:number = 0,perPage:number=10){
+    let params = new HttpParams()
+                    .set('page',page)
+                    .set('perPage',perPage);
+    return this.http.get<any>(`${apiUrl}s`,{params:params})
+  }
+
+  getUserById(id:string){
+    let params = new HttpParams().set('user_id',id)
+    return this.http.get<any>(apiUrl,{params:params})
+  }
+}
